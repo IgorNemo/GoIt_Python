7750ab0457baaaf92ddfc304ad920621c25562d0
IgorNemo <ig.dan2020@gmail.com>
2026-03-01 16:26:50 +0200

    Start_Module3_01.03.2026

--- 1_18.02.2026_Start/1.py
+++ 1_18.02.2026_Start/1.py
@@ -1038,3 +1038,100 @@
 # print(first(1, "Alex", "Boris"))             
 # print(second(3, comment_one="first", comment_two="second", comment_third="third"))
 # print(second(10, comment_one="Alex", comment_two="Boris"))
+
+# import datetime
+# now = datetime.datetime.now()
+# print(now)
+
+# from datetime import datetime
+
+# current_datetime = datetime.now()
+
+# print(current_datetime.year)
+# print(current_datetime.month)
+# print(current_datetime.day)
+# print(current_datetime.hour)
+# print(current_datetime.minute)
+# print(current_datetime.second)
+# print(current_datetime.microsecond)
+# print(current_datetime.tzinfo)
+
+# from datetime import datetime
+
+# current_datetime = datetime.now()
+# print(current_datetime.date())
+# print(current_datetime.time())
+
+
+
+# import datetime
+
+# # Створення об'єктів date і time
+# date_part = datetime.date(2023, 12, 14)
+# time_part = datetime.time(12, 30, 15)
+
+# # Комбінування дати і часу в один об'єкт datetime
+# combined_datetime = datetime.datetime.combine(date_part, time_part)
+
+# print(combined_datetime)  # Виведе "2023-12-14 12:30:15"
+
+
+# import datetime
+
+# # Створення об'єкта datetime з конкретною датою
+# specific_date = datetime.datetime(year=2020, month=1, day=7)
+
+# print(specific_date)  # Виведе "2020-01-07 00:00:00"
+
+
+# import datetime
+
+# # Створення об'єкта datetime з конкретною датою і часом
+# specific_datetime = datetime.datetime(year=2020, month=1, day=7, hour=14, minute=30, second=15)
+
+# print(specific_datetime)  # Виведе "2020-01-07 14:30:15"
+
+# import datetime
+
+# # Створення об'єкта datetime з конкретною датою і часом
+# specific_datetime = datetime.datetime(2020, 1, 7, 14, 30, 15)
+
+# print(specific_datetime)  # Виведе "2020-01-07 14:30:15"
+
+
+# from datetime import datetime
+
+# # Створення об'єкта datetime
+# now = datetime.now()
+
+# # Отримання номера дня тижня
+# day_of_week = now.weekday()
+
+# # Поверне число від 0 (понеділок) до 6 (неділя)
+# print(f"Сьогодні: {day_of_week}")  
+
+
+# from datetime import datetime
+
+# # Створення двох об'єктів datetime
+# datetime1 = datetime(2023, 3, 14, 12, 0)
+# datetime2 = datetime(2023, 3, 15, 12, 0)
+
+# # Порівняння дат
+# print(datetime1 == datetime2)  # False, тому що дати не однакові
+# print(datetime1 != datetime2)  # True, тому що дати різні
+# print(datetime1 < datetime2)   # True, тому що datetime1 передує datetime2
+# print(datetime1 > datetime2)   # False, тому що datetime1 не наступає за datetime2
+
+
+import datetime
+
+# # Створення об'єктів date і time
+# date_part = datetime.date(2023, 12, 14)
+# time_part = datetime.time(12, 30, 15)
+
+# # Комбінування дати і часу в один об'єкт datetime
+# combined_datetime = datetime.combine(date_part, time_part)
+
+# print(combined_datetime)  # Виведе "2023-12-14 12:30:15"
+
