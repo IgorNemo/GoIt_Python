588837527d9cf75eac58df87588a906b83f79ada
IgorNemo <ig.dan2020@gmail.com>
2026-03-08 00:29:46 +0200

    07.03.2026_Автоперевірка#4_Модуль3

--- 1_18.02.2026_Start/1.py
+++ 1_18.02.2026_Start/1.py
@@ -1955,3 +1955,70 @@
 # prepared_users = prepare_user_list(users)
 # for user in prepared_users:
 #     print(user)
+
+# from datetime import datetime, timedelta
+
+
+# def string_to_date(date_string):
+#     return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+
+# def find_next_weekday(start_date, weekday):
+#     date_ahead =  weekday - start_date.weekday()
+#     if date_ahead > 0:
+#         next_weekday = start_date + timedelta(days = date_ahead)
+   
+#     else: next_weekday = start_date + timedelta(days = (date_ahead + 7))
+#     return next_weekday
+    
+# date_string = "2020.01.01"
+# weekday = 6
+# start_date = string_to_date(date_string)
+
+# print(start_date)
+# print(find_next_weekday(start_date, weekday))
+
+# from datetime import datetime, date
+
+
+# def string_to_date(date_string):
+#     return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+
+# def date_to_string(date):
+#     return date.strftime("%Y.%m.%d")
+
+
+# def prepare_user_list(user_data):
+#     prepared_list = []
+#     for user in user_data:
+#         prepared_list.append({
+#             "name": user["name"], "birthday": string_to_date(user["birthday"])
+#         })
+#     return prepared_list
+
+
+# def get_upcoming_birthdays(users, days=7):
+#     upcoming_birthdays = []
+#     today = date.today()
+    
+#     for user in users:
+#         this_year = user["birthday"].replace(year=today.year)
+#         delta = (this_year - today).days
+
+#         if 0 <= delta <= days:
+#             upcoming_birthdays.append({
+#                 "name": user["name"],"congratulation_date": date_to_string(this_year)
+#             })
+
+#     return upcoming_birthdays
+
+# users = [
+#     {"name": "Bill Gates", "birthday": "1955.3.12"},
+#     {"name": "Steve Jobs", "birthday": "1955.3.10"},
+#     {"name": "Jinny Lee", "birthday": "1956.3.22"},
+#     {"name": "John Doe", "birthday": "1985.01.23"},
+#     {"name": "Jane Smith", "birthday": "1990.01.27"}
+# ]
+# prepared_users = prepare_user_list(users)
+# print(get_upcoming_birthdays(prepared_users, days=7))
