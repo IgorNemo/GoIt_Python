5f479e7e853077a19e08de1e3383e99e6dd33d46
Igor Danyliuk <danig@ukr.net>
2026-04-08 23:20:55 +0300

    Module4_HW1

---
+++ Homework/goit-algo-hw-04/Module4_HW1.py
@@ -0,0 +1,18 @@
+def total_salary(path):
+    try:
+        total = 0
+        count = 0
+        with open(path, "r", encoding="utf-8") as fh:
+            for line in fh:
+                # Розділяємо рядок по першій комі
+                name, salary = line.strip().split(',', 1)
+                total += int(salary)
+                count += 1
+        average = total / count if count > 0 else 0
+        return (total, average)
+    except FileNotFoundError:
+        print("Файл не знайдено")
+        return None
+    
+result = total_salary("/Users/markspc/Learning/03_Projects/IgorNemo-goit-algo-hw-04/GoIt_Python/Homework/goit-algo-hw-04/file.txt")
+print(result)
