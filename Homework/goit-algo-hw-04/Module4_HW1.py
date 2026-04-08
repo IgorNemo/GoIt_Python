789697fe3b872973d97d35c4ce14b01bb8e971ca
Igor Danyliuk <danig@ukr.net>
2026-04-08 23:51:35 +0300

    Modele4_HW1_2

--- Homework/goit-algo-hw-04/Module4_HW1.py
+++ Homework/goit-algo-hw-04/Module4_HW1.py
@@ -9,7 +9,7 @@ def total_salary(path):
                 total += int(salary)
                 count += 1
         average = total / count if count > 0 else 0
-        return (total, average)
+        return (print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}"))
     except FileNotFoundError:
         print("Файл не знайдено")
         return None
